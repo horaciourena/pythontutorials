0ade582da0ec2d8042c263e52df8da1a8556260c
luis <horaciourena@hotmail.com>
2020-08-11 11:20:41 -0400

    como crear nombres de usuarios de forma aleatoria desde un website con palabras existentes

---
+++ usuarios_aleatorios.py
@@ -0,0 +1,10 @@
+import requests
+from random import randint
+url = 'https://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain'
+datos = requests.get(url)
+texto = datos.text
+#print(texto)
+palabras = texto.split()
+#print(palabras)
+num_aleatorio = randint(0,len(palabras))
+print(palabras[num_aleatorio]+str(num_aleatorio))
