5c13642bada21093bc9f0fedb75729cced34e8f2
luis <horaciourena@hotmail.com>
2020-08-16 15:42:15 -0400

    Sentencias de control IF, ELIF, and ELSE

---
+++ sentencia_if_elif_else.py
@@ -0,0 +1,19 @@
+# if (3>2):
+#     print(2>2)
+# if (1>3):
+#     mayor = True
+# else:
+#     mayor = False
+# print(mayor)
+# horario_solicitado = "Dia"
+# if(horario_solicitado == "Dia"):
+#     print("Felicidades, seras entrevistado muy pronto!")
+# else:
+#     print("Lo sentimos, por el momento no tenemo vacantes disponibles en el horario seleccionada. Vuelve a intentarlo mas adelante.")
+horario_solicitado = "Tarde"
+if(horario_solicitado == "Dia"):
+    print("Felicidades, seras entrevistado muy pronto!")
+elif (horario_solicitado == "Tarde"):
+    print("Felicidades, Eres afortunado. Solo nos queda un copo disponible, seras entrevistado muy pronto!")
+else:
+    print("Lo sentimos, por el momento no tenemo vacantes disponibles en el horario seleccionada. Vuelve a intentarlo mas adelante.")
