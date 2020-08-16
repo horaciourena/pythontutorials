1d7a43d4a198b2a27cb4c43d46e8f1e2eb5e51ba
luis <horaciourena@hotmail.com>
2020-08-16 10:40:21 -0400

    algoritmo para numeros pares e impares

---
+++ numeros_pares.py
@@ -0,0 +1,6 @@
+num = int(input("Digite un numero: "))
+#print(type(num))
+if(num%2==0):
+    print("El numero:", num, "es par.")
+else:
+    print("El numero:", num, "es impar.")
---
+++ sets.py
@@ -0,0 +1,9 @@
+#Sets en python
+
+miset = set()
+miset.add(1)
+miset.add(False)
+miset.add("Dia")
+milista = [1,1,1,1,4,4,4,3,3,3,1,2,2,4,5]
+newSet = set(milista)
+print(newSet)
